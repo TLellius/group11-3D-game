238baecd18a6763b6d5671332a7960b2a1f0c94c
chunkmega <94521970+chunkmega@users.noreply.github.com>
2026-04-25 17:51:39 -0700

    Added Sprout run animation
    added Sprout run animation in "anim-sprout scene"

---
+++ Assets/Scripts/SproutAnimation.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SproutAnimation : MonoBehaviour
+{
+    Animator animator;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0 || Input.GetAxisRaw("Horizontal") < 0 || Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("Vertical") < 0  )
+        {
+            animator.SetBool("isMovingSprout", true);
+        }
+        else
+        {
+            animator.SetBool("isMovingSprout", false);  
+        }
+        
+
+
+
+    }
+}
